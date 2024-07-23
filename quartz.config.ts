4b2f2f3c2727fb2e449f6d7f1c291d41eeac5e52
Esbern Holmes <holmes@ruc.dk>
2024-07-23 14:16:53 +0200

    Quartz sync: Jul 23, 2024, 2:16 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 GIS garden",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
